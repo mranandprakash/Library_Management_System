d71ea2a05b1c13c4bc338087462361039ff42aa7
Deepak <deepaklewiskumar.19@gmail.com>
2025-09-10 23:46:36 +0530

    add-book and render book

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let books=[];
-let users=[{userid:"john123",password:"pass1234"}];
+let books=[
+ { id: "b1", title: "Introduction to Algorithms",author:"s morley",category:"computer science", available: true },
+{ id: "b2", title: "Clean Code",author:"rd barman",category:"computer science", available: true },
+{ id: "b3", title: "The Great Gatsby",author:"davind finch",category:"fiction", available: true }
+];
+let users=[{userid:"john123",password:"pass1234",usertype:"student"},
+    {userid:"deepak",password:"pass123",usertype:"admin"}
+];
 let currentuser=null;
 function addbooks(){
     const title=document.getElementById("title").value;
@@ -7,6 +13,7 @@ function addbooks(){
     const category=document.getElementById("Category").value;
     const avail=document.getElementById("avail").value;
    const newbook={
+    id:"b"+(books.length+1),
     title:title,
     author:author,
     category:category,
@@ -31,6 +38,21 @@ function renderbooks(){
         tbody.appendChild(row);
     })
     
+}
+function handlelogin(){
+    const userid=document.getElementById("userid").value;
+    const password=document.getElementById("password").value;
+    const user=users.find(t=>userid==t.userid && password==t.password);
+    const login=document.getElementById("login");
+    const admindashboard=document.getElementById("admindashboard");
+    const tablediv=document.getElementById("tablediv");
+    if(user){
+        login.style.display="none";
+        admindashboard.classList.remove("hidden");
+        tablediv.classList.remove("hidden");
+
+    }
+
 }
 
 
